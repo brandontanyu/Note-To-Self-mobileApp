6462cc6f75a80507400e8e4d87f97b98f94986eb
brandontanyu <brandontanyu876@gmail.com>
2023-05-20 21:44:04 +0100

    added codes

---
+++ app/src/main/java/com/civilsalt/selfnote/DialogNewNote.java
@@ -0,0 +1,36 @@
+package com.civilsalt.selfnote;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class DialogNewNote extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_new_note, null);
+
+
+        final EditText editTitle = dialogView.findViewById(R.id.editTitle);
+        final EditText editDescription = dialogView.findViewById(R.id.editDescription);
+        final CheckBox checkBoxIdea = dialogView.findViewById(R.id.checkBoxIdea);
+        final CheckBox checkBoxTodo = dialogView.findViewById(R.id.checkBoxTodo);
+        final CheckBox checkBoxImportant = dialogView.findViewById(R.id.checkBoxImportant);
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+        Button btnOK = dialogView.findViewById(R.id.btnOk);
+
+
+    }
+}
