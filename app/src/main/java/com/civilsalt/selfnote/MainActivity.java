adc9625d58fa97b5f3f997fb50dcec1ee91ea711
brandontanyu <brandontanyu876@gmail.com>
2023-05-21 14:52:47 +0100

    added content_main.xml file to hold widgets for the activity_main.xml resource file

--- app/src/main/java/com/civilsalt/selfnote/MainActivity.java
+++ app/src/main/java/com/civilsalt/selfnote/MainActivity.java
@@ -3,13 +3,36 @@ package com.civilsalt.selfnote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // Temporary code
+    Note mTempNote = new Note();
+
+    public void createNewNote(Note n){
+// Temporary code
+       this. mTempNote = n;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Temporary code
+        Button button =(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        // Create a new DialogShowNote called dialog
+                DialogShowNote dialog = new DialogShowNote();
+        // Send the note via the sendNoteSelected method
+                dialog.sendNoteSelected(mTempNote);
+        // Create the dialog
+                dialog.show(getSupportFragmentManager(), "123");
+            }
+        });
 
 
     }
