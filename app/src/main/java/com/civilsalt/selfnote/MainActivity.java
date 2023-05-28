be85a09fafad58e26db94ab333c3b408aa7be8ce
brandontanyu <brandontanyu876@gmail.com>
2023-05-28 19:55:20 +0100

    coded the showNote method and setting the recyclerview inside the main activity

--- app/src/main/java/com/civilsalt/selfnote/MainActivity.java
+++ app/src/main/java/com/civilsalt/selfnote/MainActivity.java
@@ -1,15 +1,33 @@
 package com.civilsalt.selfnote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    public void createNewNote(Note n){
+    private List<Note> noteList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private NoteAdapter mAdapter;
 
+    public void createNewNote(Note n){
+        noteList.add(n);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void showNote(int noteToShow){
+        DialogShowNote dialog = new DialogShowNote();
+        dialog.sendNoteSelected(noteList.get(noteToShow));
+        dialog.show(getSupportFragmentManager(), "");
     }
 
     @Override
@@ -18,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         DialogNewNote dialog = new DialogNewNote();
         dialog.show(getSupportFragmentManager(), "");
+
+        recyclerView  = findViewById(R.id.recyclerView);
+        mAdapter = new NoteAdapter(this,noteList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        // Add a neat dividing line between items in the list
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        //set Adapter
+        recyclerView.setAdapter(mAdapter);
 
 
     }
